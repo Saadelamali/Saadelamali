828700d5c933e1a36689285d97fdce0aef3782bf
Saadelamali <83782029+Saadelamali@users.noreply.github.com>
2021-05-29 15:39:50 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,66 +22,23 @@ client = commands.Bot(command_prefix = '.')
 client.remove_command('help')
 token = 'ODM1NTI2OTI1MTI3NTE2MjIx.YIQvFg.hdn7b7WgAI8S3-1XDeWYTXWd3Oo'
 
-@client.event
+class BotData:
+    
+     def __init__(self):
+         self.welcome_channel=None
+         self.goodbye_channel=None
+         self.reaction_role=None
+         self.reaction_message=None
+        
+botdata=BotData
+
+@bot.event
 async def on_ready():
-    activity = discord.Game(name=".commands", type=3)
-    await client.change_presence(status=discord.Status.online, activity=activity)
-    print("iam ready!")
-
-
-@client.command()
-async def destroy (ctx,message):
-    guild=ctx.message.guild
-    await ctx.message.delete()
+    print("Your bot is ready!!")
     
-    for channel in list(ctx.message.guild.channels):
-        try:
-            await channel.delete()
-            print(f"{channel.name} has been deleted")
-        except:
-             pass
-    for i in range(1):
-        await guild.create_text_channel(f"{message}")
-    while True:
-        for channel in guild.text_channels:
-            for i in range (1):
-                await guild.create_text_channel(f"{message}")
- 
-
-@client.command()
-async def s(ctx, *, message ):
-  guild=ctx.message.guild
-  await ctx.message.delete()
-  for i in range(2):
-    print("spammed")
-    while True:
-           for channel in guild.text_channels:
-                 await channel.send(f"{message}")
-
-@client.command()
-async def cda(ctx):
-    guild=ctx.message.guild
-    await ctx.message.delete()
-    
-    for channel in list(ctx.message.guild.channels):
-        try:
-            await channel.delete()
-        except:
-            pass   
 
 
-@client.command()
-async def commands(ctx):
- 
-  embed=discord.Embed(title="Commands",timestamp= ctx.message.created_at,color=discord.Color.blue(),inline=False)
-  embed.add_field(name="**.cda**",value="cda command for deleting all channels only", inline=False)
-  embed.add_field(name="**.destroy (message)**",value="destroy command for deleting all and creating 500 channels ",inline=False)
-  embed.add_field(name="**.s (message)**",value="s command for spam the bot will send your message in all channels in the guild",inline=False)
-  embed.set_footer(text="developed by Saad.")
-
-  await ctx.send(embed=embed)
-
-@client.command()
+@bot.command()
 async def dmall(ctx, *,args=None):
   if args !=None:
       members=ctx.guild.members
