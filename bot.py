94b312c09cd880cd37b5f23a90df4902677b15dc
Saadelamali <83782029+Saadelamali@users.noreply.github.com>
2021-05-31 18:25:02 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,24 @@
- discord.utils import time_snowflake
+from logging import error
+from operator import mod
+import re
+import random
+import discord
+from discord import embeds
+from discord import colour
+from discord import user
+from discord import member
+from discord import permissions
+from discord import emoji
+from discord import mentions
+from discord.colour import Color 
+from discord.ext import commands
+from discord.ext.commands import bot
+from discord.ext.commands.bot import Bot
+from discord.ext.commands.core import after_invoke, command, cooldown, has_permissions
+from discord.ext.commands.errors import BotMissingPermissions, MissingPermissions, MissingRequiredArgument
+from discord.member import Member
+from discord.shard import AutoShardedClient
+from discord.utils import time_snowflake
 from discord import Intents
 from discord.ext import commands
 
