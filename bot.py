21dc751ca683d65273265770eaa1b9f17cd51b6d
Saadelamali <83782029+Saadelamali@users.noreply.github.com>
2021-05-31 19:08:20 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,7 +18,10 @@ from discord.utils import time_snowflake
 from discord import Intents
 from discord.ext import commands
 
-client = commands.Bot(command_prefix = '.')
+intents = discord.Intents.all()
+
+
+client = commands.Bot(command_prefix = '.',intents=intents)
 client.remove_command('help')
 token = 'ODM1NTI2OTI1MTI3NTE2MjIx.YIQvFg.hdn7b7WgAI8S3-1XDeWYTXWd3Oo'
 
