30135d4770bb8a8b8472dff3cbef738cd80ed6f0
Saadelamali <83782029+Saadelamali@users.noreply.github.com>
2021-05-31 18:53:15 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 from discord import Intents
 
-intents = discord.Intents.all()
+intents = Intents.all()
 client = commands.bot(command_prefix="!" , intents = intents)
 
 @client.event
