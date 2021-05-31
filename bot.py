4ab78215834318abf0410d614c8e06d4ccb00ab9
Saadelamali <83782029+Saadelamali@users.noreply.github.com>
2021-05-31 18:24:34 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,29 +1,10 @@
-from logging import error
-from operator import mod
-import re
-import random
-import discord
-from discord import embeds
-from discord import colour
-from discord import user
-from discord import member
-from discord import permissions
-from discord import emoji
-from discord import mentions
-from discord.colour import Color 
-from discord.ext import commands
-from discord.ext.commands import bot
-from discord.ext.commands.bot import Bot
-from discord.ext.commands.core import after_invoke, command, cooldown, has_permissions
-from discord.ext.commands.errors import BotMissingPermissions, MissingPermissions, MissingRequiredArgument
-from discord.member import Member
-from discord.shard import AutoShardedClient
-from discord.utils import time_snowflake
+ discord.utils import time_snowflake
 from discord import Intents
 from discord.ext import commands
 
+intents=discord.intents.all
 
-client=commands.bot(command_prefix=".")
+client=commands.bot(command_prefix=".",intents=intents)
 client.remove_command("help")
 token="ODQyMTQ2ODcwNjk1ODIxMzI0.YJxEZA.7hAolYuevVbG1VgpHw-FlZYtBj0"
 
@@ -33,15 +14,13 @@ async def on_ready():
   print("im ready..")
 
 @client.command()
-async def dm(ctx, *,message):
-    guild=ctx.guild
+async def dm(ctx,args=None):
+    if args !=None:
     members=ctx.guild.members
-    await ctx.delete.message()
-    
-    for members in list(ctx.guild.member)
-    try:
-        await member.send(f"{message}")
-        print("message sent to"+member.name)
+    for member in members:
+      try:
+          await member.send(args)
+          print("message sent to "+member.name)
     except:
          print("I cannot send this message to "+member.name)
 
