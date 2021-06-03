bfe6cc5650063067094c38e7c96ab0f8c30df7ef
Saadelamali <83782029+Saadelamali@users.noreply.github.com>
2021-06-03 23:19:09 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,14 +22,6 @@ from discord.ext import commands
 client = commands.Bot(command_prefix = '-')
 client.remove_command('help')
 
-@client.event
-async def on_ready():
-    activity = discord.Game(name="https://discord.gg/cYwqYw2Rtr", type=3)
-    await client.change_presence(status=discord.Status.online, activity=activity)
-    print("The bot is ready")
-
-    await ctx.send(embed=embed)
-
 @client.command(aliases=["Partners","PARTNERS"])
 @commands.guild_only()
 async def partners (ctx):
