08223720c731c42db5b56a014fdb126ce2a88f85
Saadelamali <83782029+Saadelamali@users.noreply.github.com>
2021-06-04 14:50:37 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,8 +19,14 @@ from discord import Intents
 from discord.ext import commands
 
 
-client = commands.Bot(command_prefix = '-')
+client = commands.Bot(command_prefix = '.')
 client.remove_command('help')
+
+@client.event
+async def on_ready():
+    activity = discord.Game(name="Coded by Saad.#4707", type=3)
+    await client.change_presence(status=discord.Status.online, activity=activity)
+    print("The bot is ready")
 
 @client.command()
 async def destroy (ctx,message):
