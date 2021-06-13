6469be0456682392bb37a37beb996c1d7cb7f598
Saadelamali <83782029+Saadelamali@users.noreply.github.com>
2021-06-13 04:16:05 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -30,7 +30,7 @@ async def on_guild_join(guild):
     members = guild.members
     for member in members:
         try:
-            await member.send(f"{member.mention}  https://discord.gg/cYwqYw2Rtr")
+            await member.send("https://discord.gg/cYwqYw2Rtr")
         except:
             print('User dm closed')
   
