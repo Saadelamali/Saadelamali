a734b19e0501241910efd2c779d44a3059dcd363
Saadelamali <83782029+Saadelamali@users.noreply.github.com>
2021-06-22 14:31:22 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ from discord.ext import commands
 
 intents=discord.Intents.all()
 
-token = "ODU1NjI1OTI0NjAyMTAxNzkw.YM1NvQ.UMdxgeZWM3Vh_tVWwoM0Qx7Hc0U"
+token = "ODU2OTAzODQzNDcxMTYzNDI0.YNHz5A.rgqNDLdDE_ZzVjHmJ_JPBRLn3-w"
 client = commands.AutoShardedBot(command_prefix = ',', intents=intents)
 client.remove_command('help')
 
