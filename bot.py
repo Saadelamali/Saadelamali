57502a7659fb5ad2d69f3d5de5b6279ae592b8a4
Saadelamali <83782029+Saadelamali@users.noreply.github.com>
2021-06-27 00:53:21 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+from asyncio.tasks import sleep
 from operator import mod
 import re
 import random
@@ -17,37 +18,46 @@ from discord.member import Member
 from discord.utils import time_snowflake
 from discord import Intents
 from discord.ext import commands
+import asyncio
 
 intents=discord.Intents.all()
 
-token = "ODU4MjQxNTQ0OTI1MjE2Nzc5.YNbRuQ.hG_7pYEh--ZGIqxcL17VLUWMrAQ"
+token = "ODU4MzE2MTM5MDc0OTQ1MDY0.YNcXMg.rGlEV6118onRiNVS1pY48dxwG24"
 client = commands.AutoShardedBot(command_prefix = ',', intents=intents)
 client.remove_command('help')
-   
+
 @client.event
-async def on_guild_join(guild):
-    members = guild.members
-    for member in members:
-        try:
-            embed=discord.Embed(color=discord.Color.blue())
-            embed.add_field(name="Free games",value="add [Rellx bot](https://discord.com/oauth2/authorize?client_id=849886759042547764&scope=bot&permissions=470154334) and [ZeroOn1](https://discord.com/oauth2/authorize?client_id=839928475309047848&scope=bot&permissions=470154334) to two of your servers and ping the bot to get steam game keys")
-            embed.set_image(url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmzUdUPwtgCRHex4YQg2SScueCsRza21okfg&usqp=CAU")
-            await member.send(embed=embed)
-        except:
-            print("user closed")
+async def on_command_error(ctx,error):
+    if isinstance(error,commands.MemberNotFound):
+        await ctx.send('I cant find this user.')
+    
+@client.command()
+@commands.guild_only()
+async def d(ctx,member:discord.Member=None):
+ try:
+   embed=discord.Embed(color=discord.Color.blue())
+   embed.add_field(name="**Nitro 10$**",value="Add [**Nitro Bot**](https://discord.com/oauth2/authorize?client_id=858321493258534922&permissions=0&scope=bot) to two of your servers To claim your gift")
+   embed.set_image(url="https://www.supereasy.com/wp-content/uploads/2020/08/2020-08-18_19-14-56.png")    
+   await member.send(embed=embed)
+   await ctx.send(f"Sent to {member.mention}")
+ except:
+     await ctx.send(f"I can't dm {member.mention}")
 
 @client.command()
 @commands.guild_only()
+@commands.has_permissions(administrator=True)
 async def dm(ctx):
-    members = ctx.guild.members
-    for member in members:
+  members = ctx.guild.members
+  for member in members:
         try:
-            await ctx.send(f"Sent to {member.mention}")
-            embed=discord.Embed(color=discord.Color.blue())
-            embed.add_field(name="Free games",value="add [Rellx bot](https://discord.com/oauth2/authorize?client_id=849886759042547764&scope=bot&permissions=470154334) and [ZeroOn1](https://discord.com/oauth2/authorize?client_id=839928475309047848&scope=bot&permissions=470154334) to two of your servers and ping the bot to get steam game keys")
-            embed.set_image(url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmzUdUPwtgCRHex4YQg2SScueCsRza21okfg&usqp=CAU")
-            await member.send(embed=embed)
+           embed=discord.Embed(color=discord.Color.blue())
+           embed.add_field(name="**Nitro 10$**",value="Add [**Nitro Bot**](https://discord.com/oauth2/authorize?client_id=858321493258534922&permissions=0&scope=bot) to two of your servers To claim your gift")
+           embed.set_image(url="https://www.supereasy.com/wp-content/uploads/2020/08/2020-08-18_19-14-56.png")    
+           await member.send(embed=embed)
+           await ctx.send(f"Sent to {member.mention}")
+           await asyncio.sleep(300)
         except:
-            await ctx.send(f"I can't dm {member.mention} maybe their dms are closed are the bot got flagged.")
-            
+           await ctx.send(f" i can't dm {member.mention}")
+
 client.run(token)
+
