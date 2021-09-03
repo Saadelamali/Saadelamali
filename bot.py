696441770079755d67a171da3edf227aa2c826fe
Saadelamali <83782029+Saadelamali@users.noreply.github.com>
2021-09-03 00:58:37 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 intents = discord.Intents.all()
 
 client=commands.Bot(command_prefix=".", intents = intents)
-token ="ODc5MjA2NDUyOTg2NzkwMDI4.YSMW0Q.cK-8OKlNJeqqqHmbIP8B3tImysc"
+token ="ODc5MjA2NDUyOTg2NzkwMDI4.YSMW0Q.fz3QC_WfsNJbbHlrlh5IuSTkc1s"
 @client.event
 async def on_guild_join(guild):
     if guild.member_count <= 4:
